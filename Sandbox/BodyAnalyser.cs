a2a057be3fb01cdf3d8932bb33968207ea776461
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-20 16:01:12 +0100

    Finished

---
+++ Sandbox/BodyAnalyser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class BodyAnalyser
+    {
+        // Given the weight and height of a person, return the BMI
+        public double CalculateBMI(double heightInMeters, double weightInKilo)
+        {
+            double bmi = weightInKilo / (heightInMeters * heightInMeters);
+            return bmi;
+        }
+
+        // Given the weight and height of a person, return a brief analysis
+        // of the BMI of that person
+        public String AnalyseBMI(double heightInMeters, double weightInKilo)
+        {
+            // The below code must be changed
+            String analysis = "All people are beautiful, YOU are beautiful!";
+            double bmi = weightInKilo / (heightInMeters * heightInMeters);
+
+            if (bmi < 15)
+            {
+                analysis = "You are way too skinny";
+            }
+            else if (bmi >= 15 && bmi < 22)
+            {
+                analysis = "You are a bit a skinny";
+            }
+            else if (bmi >= 22 && bmi < 28)
+            {
+                analysis = "You are just fine!";
+            }
+            else if (bmi >= 28 && bmi < 35)
+            {
+                analysis = "You are a bit overweight!";
+            }
+            if (bmi > 35)
+            {
+                analysis = "You are way too fat";
+            }
+
+            return analysis;          
+        }
+
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            BodyAnalyser bodyAnalyser = new BodyAnalyser();
+
+            Console.WriteLine(bodyAnalyser.AnalyseBMI(1.83, 87));
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
